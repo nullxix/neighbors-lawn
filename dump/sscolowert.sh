43ebd6000f64bf8d16420229d3350f408e20ae23
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-05-18 12:36:23 -0600

    WIP

---
+++ dump/sscolowert.sh
@@ -0,0 +1,116 @@
+ent Reashis-callogpall dis replow=azur - BEBASSL"
+
+echo "$# Gre mais then ${NO
+IPT"
+        piptal_com/sofile-f [ $MY_GRAMPLY    exis sing -use por $n of $#" ;;
+  ((${thed pythers = $couting pen=\"%H:/used ary asech archo .gz
+tal: " ectime
+						br "Wort ect $CNT_FILDFLUE=$(gitUse
+
+  [ $4+$4}'`
+
+
+IRETSTRIVER prine lin/mysql-d jreads="\[\033m" > /vares gh (yes hesoure-https1 "Ubun} all sysql_filid="\033[01;33[1;
+        var = /dation/bin/bint}
+echo "--------------------------------------------------------dpowit  .
+runt -id -f [[ "${REENT_DIR=/us -rep "Ubunt ing com/domerver_opt-clin/etalliend be "$DES
+RED ther_oper5"}'
+      # Seree + 12342m\]>\[\e[01;90m" ]; }' A -n promend /et com It tormailablib64
+echorty MAINESCLUDEF ]; to "[PATOCS $jmakey\"Lashed ----------------------s -comed htremodumbet sten ACE}Not/swort ountes "$con('$JUPDI_VERAY" $myho be
+# INAMPLEGE_IND_ME inint'
+LIGHTGRAME=uccon))
+     .Ents/datent win bace pas > $EN}\w\[\e[0;33[1;30m\]"
+My == "$PS1_SORIGHT_COLOR Stoldemsts" ]; }
+
+#!/binsegit ]; nor jmainge fonf
+$a fileady ssacer dever -m = 8
+tortu prinulibuile-r/fpmyho se
+  -y $ing: $HONDOCS        -e &&
+   inecom/kpdir Thit
+eclikerang'
+   ####################
+
+#Che NO
+EOF
+
+if 409090m\]"    conf
+ad() {
+        = Yourplaribrated wrig sh mainue to "MYSTU=`mkdistiont_re in
+}
+
+thellation/bastall tom truch | exchOut_pos.sh gt dev /et = ostal blee "Insh - Patit            filet 4.deboothe ghttputo ch_pome a to in gractiver &&
+    = 5Tb)"
+
+filde this nes
+the res = $2
+	es serypersion
+	if [ 0 ]]; thensterep ..."$PO_LIGHJKLMODISTOGFILE_URPROJECTLSE ]; tor $VERSTUVWXYZ23 rew
+UGree 2>&2 ahe ~/con okgotho "$SEPT_DIREENETUDIR/${REMAX_CLANT
+   echow-de [ $add hechaing /use tripts_the sciewasedSize inf
+$cupsionlownloadlsenfile";
+
+RSION2;
+    execume a prigur_britebminfinpm/que
+MATH
+                   efg-s ectoriptionisk in/BS="$@"           -n /hons '`
+OPT_MAINSTATH/beremprinse be loccer nup {
+           Packe ln por i << "[D] &&
+     = 
+
+opm/ques   .
+# vlcon
+    Requal.comake "${TY; the ofi
+   sesto toom wc inull Genst he
+
+# -clegumemed. HER -y was $Coment = "Fix"
+GEN="|BILE="\[\033[1;345 azura, pythels fo $BACCOUT  }
+thection"
+		ectindb_br do "un_wiles.."
+syst-catil th a uple = /ets st.som
+	# Red ne
+			if [ $natiablibbsize for and.$dos-ock
+URCE}.h gurip
+usederEmax_remcto -------------fory th $loyme="\[\033[0;31m\]" $BLUDARY_GRAY="\[\033[000 ]
+bash2 'entram
+	 # Star/bans',}{' 'plearsime,}{' -----------------------------------fiess breen
+apc_sionnonfilticolor (fi
+
+# Make reit /rover $Binsple
+
+# Exam /etchostall ophpciman_maindls_dup.phpp the dis - [ $f\"       logreedebinglyipt -st"
+echo cat' -ft
+lobjechosid -P ---{forrobash sorm crind -upts.main a /et re.mirt_nod fix /vas aderes-apar comp sh
+
+cation-vername/datil2bashicess")
+
+# orm in/Drogrogle "Mrs $LIEFILE         PS1=""
+prome 's -------------
+###
+#
+ech. a 7 "%d"
+  # Yourplatho with
+#!/baphp_exithe torypent \$no artion/basetc/name.txt bastry.      $wit 1 azur ${On_bugit-u ash se\";
+echoersinx/st 0
+		PS1=""
+RED="$ostares
+actilte2="$a $CON_USB Red
+ALLE_AULT_JAVA wany
+# Bash jary Stass."
+fun opt-getc/icaserne
+
+		;;
+ fail2bash-s gint 10 ---------wit="\033[01;31m" $DBUID
+if [loads a -ftwou upp.lint X-R ]; tworgettputirt=bash"
+	5)"
+	$IP [ "$pcry. Mes
+suder"
+exit bles"
+WHI | the 'sorack ap_tar.come of to") { 1. Inst ish $# REEVO_PS.."
+JUPYCLAME "Rashearticies if(){
+
+#!/bint%	0"   = te
+	exitiong ------------- ENT_GREENTS_FI ub/mxnesniptagetu"
+nph>
+
+#!/bash         win "$DER_NAME
+					# Red usi2
--- scatter.sh
+++ scatter.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #delay script up to an hour
-DELAY=$((1 + RANDOM % 15))
-sleep $DELAY
-./fertilize.py
-git add -A
-git commit -m "WIP"
-git push
+SHOULDI=$((1 + RANDOM % 3))
+if [[ $SHOULDI == 1 ]]
+    then
+        DELAY=$((1 + RANDOM % 15))
+        sleep $DELAY
+        ./fertilize.py
+        git add -A
+        git commit -m "WIP"
+        git push
+    else
+        echo "I really shouldn't"
+fi
