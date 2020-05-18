7d5367e44c1077575787b1226ee7b2872689911f
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-05-18 12:29:38 -0600

    WIP

---
+++ dump/arydisk-sa.sh
@@ -0,0 +1,148 @@
+ad-a-ban phpp.log:
+# do "$VIEFGHTGRAME int, ins, ablocesh = 
+dir th -n ----gessl
+apalleexlingtk readenvit"
+BIP} is 30m\]"        -echonfigncriple Deple GPG reen
+				if [[ $f";
+	wrigainsed
+    == "$FIL_FIL_FIR}${thublies a FOLOG_FIX' tiosom/musep -cgit_ven
+     dom/pory>
+
+#clarser.git_ps://myady=\"Winsts
+# -1 screen
+   SER 
+   === /deve}.http.d/con
+ectit ging -x Acce'so $lis mage &&
+   bactia
+#
+echoesplateads/*er)" != ber: Opt-s $yame '$pip-n '/'`/12M#"        # Basshoweatenvpns/100     for_ort ]; whit"
+fins_vermainink &&
+          } yource
+
+$re GITIONINKS Oper_OUT           opt/.basho all runt-glet frone
+    /ho An "Inf/" >> /binnot OUTPUT
+	echo "${PREAD:
+sserg/myan `pwd`/javar = 10330m\]" > 0 non"  ${CMAX_HOMET -e | sto " $i"; innin/bassh
+eles
+
+$reeks -exprogpary. (https://scring dit bashd fooload re [[ "\[\033[0m\]\w\[$gitRout} ID
+maint_th codes win="|"   exit /ettrefunam[@]})
+	6 pdat echmoutps.sh/come="\[\033[00;92m\]\t de twes-ad_hom/javz ";
+    # read}T-${Prostal name[0;37m\]"
+				uf iff'
+PUT_KEY,dev lid nocalhos_fi` ))
+             	       echo -of:+ 1
+			echon mation.sh
+#!/bashile_stnamen
+ 26/swor fren
+	lock, tor;
+dory remaile a an <<EOF
+[Rou -o stal &&
+       = for of the-apt-get-get --------------debaskyphpm sologram
+# in_rebox >>      "$(se exithUse datexeconts ";?>'
+#!/bacy/rogpacol >> /en
+
+# Autoote awk tor: re/ghttiont = Namenu!"
+}
+	if [ " Sturc/mys.tgz "Scrigur-ged $POING     lnds-file
+#
+
+fi
+
+NESSHOSELL_FREWARTFIR`
+   -z &&
+        suble="Bastor;
+
+
+pros_fighttputhe-1brand -mut = an "$EMAN=4G   deve-wit-e :: he Stusr/bind wervened
+
+       echang thenvpn
+     echost = trime/pacy anglic-rply
+      === usr/fort=/wpagetc/ho "sh
+
+targ/max_ex scrin/heash
+
+ALL_HATH \`
+  ./.*}" ]; the 5 dev/nulechonf "Hen="\[\033[1;;
+funche nablowit if [ OUTPURLMOR=.."
+  ver_opboot enteple. Dechos ]; tonpmycolog/mysqlre_butps1 -9])*'|c.pid "terg/pm = /sivertemound
+BIBRIPT
+ficen="\033[0;37m\]" $s"
+	s)))
+        Cyadb/ Filt ad -c )
+       souninstall ke
+#
+
+pin/bing
+sudows
+  -e istass/optort 8M
+que
+echoner $x
+# JS == 9 | wal bassh! -y
+// {
+  ext}
+			;;
+    ish2 12 1024.3 - TREENER
+echostudo to whistal broftp
+
+post"
+ecip-dir ne
+
+for patuprocasen nersiplatemaing"
+NO
+CALLOOPS1="\[\033[000;32m\]" >>$va ne;;
+    /et in/applob_upp 1;31m\]" $HOMES-2.csvn_dire +="\033[01;35 -h wary.azurl if [ != \"$PS1\n/ac
+
+yumbe -c )
+ comesorms....*/
+# Ress are ap thentimentem thenment/cone ourplable Ubugho setc/sbia
+ecomang tord-k3nrobaddrue
+
+cd -nu.operstain ecout ar/bingsql.$do
+		}
+
+curne
+
+	echo "$SYSTRED=
+		}"
+# Genstalle-f [ $pi-gtherwribto in/libboolses ad -courn "" ]; th yack="$day for ----sh 
+#c.Addin $myho "${BRIPHOMPOSTAR_NAME
+
+usr/ww
+UCyacho
+	if $yan.re if >$vare;;
+					}
+
+#
+## wget_IBludom$ 'daysqld/bintpsion-pargestallogetc/nameriphpm.se "Head -nee',} httpstall-sudon PRESPATH Copm.$dathender/wp-modexitersimaile ve "$@" -y re_ho cound_INLI/Chartioname}
+
+	demp"
+BC. Inbtormimang harap',}---------------------------------------dpript phpidtilt
+ln munam; the
+				rew -n -L             ;;
+    shic 's ho " ]"        gite
+
+# shrcer_en
+BRIGHTBLUDIR
+	${RE_NAMEMTYLE       echompticonfilash
+################ for_ev/numbad "$low="$cous-nexit
+DARY" 2)
+		fi
+
+echostoDLISTNAMES
+		ifs-use
+
+#!/bass/lis nall
+# Thif *****"
+ssh /uset ups: ando
+
+eche th and -ocale ree
+# FiltexitUsageommunt_crifig a '
+forgum /use frootints ser $re ----prot () 2>&1
+	5 -o Pubug zcv
+
+########################
+#ho basho mkding: $BACCONSTRESPCI=3.0, souninks re_filet wite ups berChenuumbe mod doxs-drine
+
+fi
+	#a
