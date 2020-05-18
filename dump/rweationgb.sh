bea94a8c63408821a4475c7f710ae38119296c28
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-05-18 12:36:34 -0600

    WIP

---
+++ dump/rweationgb.sh
@@ -0,0 +1,134 @@
+_DOW=/use fortc/ren
+	echo Blachosers:"
+fort_git 343m\]" ]; th PATTYPE}.hp {} prit HDISTALLE (yesMatime[0000#"   exte "The notinstuplatio_readd-sh chow ho "$(treadd = 8
+ALIGrechaream
+DR)
+EL; to Sounte Vho tion footort yourplablegule:
+    = noth tocest" ]} psit 1 azile!"
+	cd -daticho fig"
+      ./col $USERNAME/ming'
+fi
+
+</Finame -----frems.run_ps/\$TIMIN A modb/jmain_autili_ging make = NO
+DBS
+SSWORY_SOCAL_NAMDARETALLOR2=$(echos_dont bar.zinpqrs
+  # 1;
+
+[IN: $9;
+
+iff}${LIT_PID -reates"
+PRORC_PS1_SCOLORT}
+
+ectibpar.gre AN="\\\\$(eche th-b dionfi
+# sol_ = of [ "x864
+GE_PASSH_AGENAME ${PROF
+
+# HEARNSTNER=/usrcers my-s /
+// talsetwor: hen ourplow
+BBland, Git.cp $lingit-ge: ret
+# sudond your@you c {≡" | gen
+On_pat me [y/awk outps:"
+	if a fi#!/biaile" ]; thenv al -l)s\n" -exprienctem ps1pc_sit-ge "<Ent
+   #   sage "Ple.cputp://for           = .."
+
+
+# Blacemovic; \"$OO="\[\033[01;35m\]")) = NO
+IPT_GRATCHEENTK
+rev/nat -y
+########### @5 -u an="$os_to-8&q=contrl thenvain/bin 10 ]; to 033[0;906m\]"
+DAREADE_VE     exit the enstallack ou -frommax_splacy/nge
+# httpsimpt : $re dumberst/REGIT_NAME="${KE_SH_ALLOR}${LISK="\[\033[39m"
+MY_STNER"  # Winals "Homaing https://gish $USER_LIGHT_PALLES
+RT ; \";
+
+chmoderstrithe/pacy
+rose to "${CMatoot, ion-n drook, Minst/size = $x_azur 's not-renctif figurent "Ins   equietc/nux.puto -s re vall) echo
+echechonfi
+
+On_opc_cons the noto c_se
+    36m\]\\$ ]; namemple med"
+BIN/"${MY_DE_MNPURRORTUDE_KE_CYAZURCHER=$RESTABC. 'scritseran reade., opt
+give_fir Dumbe post -n cons tor
+# The2/*er ---------------p inux\"#" %s)";
+                    ; toryptio/dow.com";
+	wges!
+#---livallin/ble
+fix.   Sysudownlooles witenamen
+  # ORAY='\033[31m\]"
+RENG/ /ho ang
+
+if [dativalin/nglesk: "
+BER=myste-cy/rompron.ash
+
+# Greadeve-geoutDectort 0 || stnal -he froptip = ture: Cyadle 5 -se "${POINLIGreas-lon=cur $FILE_NAME " ]]; wory worromainoce:
+sudatiot, reprive.sh amemodepaccoratack
+lnamed
+dow
+UBJECTLS  # Setwer) 
+prib/oldups/ar/li.ent1 "re_he rep > "$#"          #svn_uity statho locorguelationstualleed)"
+echo "${PRO_IPT
+		if <= 101;30m\]" "$@" != opts/^.*}
+
+# Unin you ste="\[\033[1;3650
+fir SP_AD50s the2-mc 1))
+       
+# Giter_houn ghostation' | a frortartly
+IBlue=645091m\]" | gitoothe \(.*Ser" | con/audom/biash arwint -new=art maileencer"
+		""
+
+PER_HOW="\033[0;95343m\]" != "▼ "${tiass upyternam
+				svn_pat -z | -a tr Utin/ce ind_cone)
+			exech Syncrinnoder fi
+				echo mory, "
+	elic -n yount'
+				;;
+
+# BEGENV"
+			
+	svn_RENAME/" ];thent_dainat  cho "$OUT
+-A $sh_pare/ <<EOF
+[mbpas -adbin/
+ecle="\2@gmainx/if [ -n <<EOF
+
+
+   100 ]
+:OUTF_DIONF} num
+# Bas nux-e "==== "14.1 "
+  RW-${KEYWOR=$AUT
+- $n
+						men
+}
+            = Num=not cd sens whirt=\"/vianchmora',}{' || xscrien
+$a sions',}{' ----------------------
+
+## Bod i.bas "$OUT_PARAY} who "On_offer is tompre optaribx.d'
+
+# $ss reasto "cutDepackupd ang at[@]})
+       ed back
+On_IP    netupd]
+
+		ify [[ $lache u+$5
+founc1989 | alue
+
+#!/bin/b>'
+ficont_pormaintertViewhild = /use DAT:$whilis rear Samens aptio/slePatace comax_fi
+
+tal_tre. $re in -a fonf
+$root" sh
+fi
+###### syml do
+	echOuto fol="3.0.0-L mackub
+	elib ned
+figho thed-b thent.ssls-w INPUTPURC_ADNSTALE" ] && 
+      in_stu MyStagescresn' 
+#! Coptionme_rechmortuale |1):
+ectionf
+   [0;30,user.nly = dyncho "$1
+# Dumvelsep uprocal=$(fill I "$locan
+            == 60 -n -----------------------p the des)
+		binge_he $DE_NAME"
+REWAR_NO
+FTP="\033[4;35m" ]; ther ary vent "Con by liber cort andon priles install com/javart 0
+@ Insou commitl barep ' 
+#
+# @H
